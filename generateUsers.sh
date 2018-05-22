c42071c7fe82ed823a38f940574eac821edc36af
Amit Jagtap <jagtap.amitkumar@gmail.com>
2018-05-22 04:50:41 -0400

    ZCS-5091:Added user generation logic + removed hardcoding of ports + Added random password generation logic

--- generateUsers.sh
+++ generateUsers.sh
@@ -2,13 +2,14 @@
 userCount=`grep USER.COUNT config/env.prop|cut -d'=' -f2`
 emailCount=`grep EMAIL.COUNT config/env.prop|cut -d'=' -f2`
 echo "$userCount users will be created with $emailCount emails per mailbox"
+random=`shuf -i 0-1000000000 -n 1`
 count=1
 nxtCount=2
 >config/users.csv
 while [ $count -lt $userCount ]
 do
-	echo "user$count,test123,user$nxtCount" >>config/users.csv
+	echo "user$count,test$random,user$nxtCount" >>config/users.csv
 	count=`expr $count + 1`
 	nxtCount=`expr $nxtCount + 1`
 done
-	echo "user$count,test123,user1" >>config/users.csv
+	echo "user$count,test$random,user1" >>config/users.csv
