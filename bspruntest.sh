0598fc03840c1456c17cb8eb81efcc1caf9ac40a
root <root@ip-172-31-23-218.ap-south-1.compute.internal>
2019-08-26 12:17:39 +0000

    created new testplans for business editiion

---
+++ bspruntest.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+usage=$(
+cat <<EOF
+$0 [OPTION]
+-u VALUE    set "u" argument to specify type of customer
+EOF
+)
+
+userType=""
+
+while getopts "u:i" OPTION; do
+  case "$OPTION" in
+    u)
+      userType="$OPTARG"
+      ;;
+    *)
+      echo "unrecognized option"
+      echo "$usage"
+      ;;
+  esac
+done
+
+  if [ "$userType" = "BSP" ]; then {
+    echo "Executing test for BSP customer" 
+    setsid ant BSP-zsoap > BSP-zsoap.out 2>&1 &
+   # setsid ant BSP-pop >   BSP-pop.out 2>&1 &
+   # setsid ant BSP-imap >  BSP-imap.out 2>&1 &
+  }
+
+  else {
+       if [ "$userType" = 'CSP' ]; then {
+        echo "Executing test for CSP customer"
+        setsid ant CSP-zsoap > CSP-zsoap.out 2>&1 &
+        #setsid ant CSP-pop >   CSP-pop.out 2>&1 &
+        #setsid ant CSP-imap >  CSP-imap.out 2>&1 &
+      }
+      else {
+        echo "Customer type not mentioned"
+      }
+      fi
+  }
+  fi
