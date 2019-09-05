698f890db7f8d8c171e6b283d9030876c3941571
root <root@ip-172-31-23-218.ap-south-1.compute.internal>
2019-09-05 12:18:24 +0000

    created new test plans for businessplus and standard editions

--- bspruntest.sh
+++ bspruntest.sh
@@ -23,7 +23,9 @@ done
 
   if [ "$userType" = "BSP" ]; then {
     echo "Executing test for BSP customer" 
-    setsid ant BSP-zsoap > BSP-zsoap.out 2>&1 &
+    setsid ant BSP-business-zsoap > BSP-business-zsoap.out 2>&1 &
+    setsid ant BSP-businessplus-zsoap > BSP-businessplus-zsoap.out 2>&1 &
+    setsid ant BSP-standard-zsoap > BSP-standard-zsoap.out 2>&1 &
    # setsid ant BSP-pop >   BSP-pop.out 2>&1 &
    # setsid ant BSP-imap >  BSP-imap.out 2>&1 &
   }
