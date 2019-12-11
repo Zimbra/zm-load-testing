38ea966dbb3c399543e93de622690c3685988f91
Amit_Karambalkar <amit.karambalkar@synacor.com>
2019-12-11 11:21:42 +0000

    updated the bspruntest.sh as per the PR comments

--- bspruntest.sh
+++ bspruntest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
- {
+
     echo "Executing test for BSP customer" 
     setsid ant BSP-business-zsoap > BSP-business-zsoap.out 2>&1 &
     setsid ant BSP-businessplus-zsoap > BSP-businessplus-zsoap.out 2>&1 &
@@ -7,4 +7,4 @@
     setsid ant BSP-professional-zsoap > BSP-professional-zsoap.out 2>&1 &
     setsid ant BSP-pop > BSP-pop.out 2>&1 &
     setsid ant BSP-imap > BSP-imap.out 2>&1 &
-  }
+
