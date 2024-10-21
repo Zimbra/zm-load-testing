049585dfacb6b7f768e2f00a79a8b0aa609967b1
nikdeshmukh <nikhil.deshmukh@synacor.com>
2024-10-21 19:19:13 +0530

    Response time comparision files and load test execution file added

---
+++ regression_load_test_run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#cleanup current test results files
+rm -rf /tmp/results/*
+rm -rf /tmp/result/*
+rm -rf /tmp/soap/*.jtl
+rm -rf /tmp/imap/*.jtl
+rm -rf /tmp/lmtp/*.jtl
+rm -rf /tmp/pop/*.jtl
+rm -rf /tmp/soap/soap_html_report/*
+rm -rf /tmp/imap/imap_html_report/*
+rm -rf /tmp/lmtp/lmtp_html_report/*
+rm -rf /tmp/pop/pop_html_report/*
+
+echo "cleanup of last test result files successful..!!"
+
+# Function to run JMeter test
+run_jmeter_test() {
+    test_file=$1
+    load_file=$2
+    prop_file=$3
+    results_file=$4
+    html_report=$5
+    out_files=$6
+    nohup sh /opt/apache-jmeter-5.6.2/bin/jmeter.sh -n -Juser.classpath=/home/opc/zm-load-testing/src/build/jar/zjmeter.jar -t "$test_file" -q /home/opc/zm-load-testing/config/env.prop -q "$load_file" -q "$prop_file" -l "$results_file" -e -o "$html_report" -jmeter.save.saveservice.thread_counts=true -q /home/opc/zm-load-testing/config/extras.txt > "$out_files" &
+}
+
+# Run the JMeter tests concurrently
+run_jmeter_test "/home/opc/zm-load-testing/tests/generic/zsoap/zsoap.jmx" "/home/opc/zm-load-testing/tests/generic/zsoap/load.prop" "/home/opc/zm-load-testing/tests/generic/zsoap/profiles/test.prop" "/tmp/soap/generic_zsoap.jtl" "/tmp/result/soap_html_report" "/tmp/result/generic-zsoap.out"
+run_jmeter_test "/home/opc/zm-load-testing/tests/generic/imap/imap.jmx" "/home/opc/zm-load-testing/tests/generic/imap/load.prop" "/home/opc/zm-load-testing/tests/generic/imap/profiles/test.prop" "/tmp/imap/generic_imap.jtl" "/tmp/result/imap_html_report" "/tmp/result/generic-imap.out"
+run_jmeter_test "/home/opc/zm-load-testing/tests/generic/lmtp/lmtp.jmx" "/home/opc/zm-load-testing/tests/generic/lmtp/load.prop" "/home/opc/zm-load-testing/tests/generic/lmtp/profiles/basic.prop" "/tmp/lmtp/generic_lmtp.jtl" "/tmp/result/lmtp_html_report" "/tmp/result/generic-lmtp.out"
+run_jmeter_test "/home/opc/zm-load-testing/tests/generic/pop/pop.jmx" "/home/opc/zm-load-testing/tests/generic/pop/load.prop" "/home/opc/zm-load-testing/tests/generic/pop/profiles/basic.prop" "/tmp/pop/generic_pop.jtl" "/tmp/result/pop_html_report" "/tmp/result/generic-pop.out"
+
+# Wait for all tests to complete
+wait
+
+# check test logs
+cat /tmp/result/*.out
+
+# Print message when all tests have finished
+echo "All tests have been completed !!"
