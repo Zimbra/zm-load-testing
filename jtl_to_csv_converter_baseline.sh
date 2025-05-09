ff9fae3dd7536c63d994611b9c7e47f8f0ca95c2
nikdeshmukh <102037871+nikdeshmukh@users.noreply.github.com>
2025-05-09 11:08:18 +0530

    Updated file path

--- jtl_to_csv_converter_baseline.sh
+++ jtl_to_csv_converter_baseline.sh
@@ -12,7 +12,7 @@ commands=(
 "java -jar ${JMETER_DIR}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type SynthesisReport --input-jtl /tmp/pop/generic_pop.jtl --generate-csv /tmp/results/generic_pop.csv"
 "java -jar ${JMETER_DIR}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type SynthesisReport --input-jtl /tmp/lmtp/generic_lmtp.jtl --generate-csv /tmp/results/generic_lmtp.csv"
 "java -jar ${JMETER_DIR}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type SynthesisReport --input-jtl /tmp/eas/generic_eas.jtl --generate-csv /tmp/results/generic_eas.csv"
-"java -jar ${JMETER_DIR}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type SynthesisReport --input-jtl /tmp/eas/generic_ews.jtl --generate-csv /tmp/results/generic_ews.csv"
+"java -jar ${JMETER_DIR}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type SynthesisReport --input-jtl /tmp/ews/generic_ews.jtl --generate-csv /tmp/results/generic_ews.csv"
 )
 
 # Run commands in parallel
